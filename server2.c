24bb256625589cb9485e01d475a1402000e4f065
Kyle Upton <kupton91@gmail.com>
2016-03-24 18:57:03 -0500

    first commit

--- server2.c
+++ server2.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <netdb.h>
 #include <netinet/in.h>
+
 #include <string.h>
 
 int main( int argc, char *argv[] ) {
    int sockfd, newsockfd, portno, clilen;
-   char buffer[256], holder[] = "";
+   char buffer[256];
+   char parser[64];
    struct sockaddr_in serv_addr, cli_addr;
    int  n;
-   
-   FILE *fp;
    
    /* Creating a socket */
    sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -22,7 +23,7 @@ int main( int argc, char *argv[] ) {
    
    /* Initializing the socket structure */
 
-   portno = 5050;
+   portno = 5015;
    
    serv_addr.sin_family = AF_INET;
    serv_addr.sin_addr.s_addr = INADDR_ANY;
@@ -52,39 +53,29 @@ int main( int argc, char *argv[] ) {
    bzero(buffer,256);
    n = read( newsockfd,buffer,255 );
    
-   //printf("Current buffer %s\n", buffer);
-   
-   if(newsockfd > 0){
-	   printf("The Client is connected...\n");	   
+   if (n < 0) {
+      printf("ERROR reading from socket");
+      exit(1);
    }
    
+   printf("Relayed  message:\n%s\n",buffer);
    
-   //open a file for reading and writing
-   fp = fopen("index.html", "w+");
-   
-   //Write data to the file 
-   //fwrite(ch, strlen(ch) + 1, 1, fp);
-   
-   /* Seek to the beginning of the file */
-   fseek(fp, SEEK_SET, 0);
-   
-   /* Read and display data */
-   fread(holder, strlen(ch)+1, 1, fp);
-   printf("%s\n", holder);
 
-   fclose(fp);  
-   
-      recv(newsockfd, buffer, strlen(buffer), 0);    
-      printf("The buffer has .... %s\n", buffer);    
-      send(newsockfd, "HTTP/1.1 200 OK\n", 16, 0);
-      send(newsockfd, "Content-length: 46\n", 19, 0);
-      send(newsockfd, "Content-Type: text/html\n\n", 25, 0);
-      send(newsockfd, "<html><body><H1>Hello world</H1></body></html>", 46, 0);
-	  //send(newsockfd, fpw, strlen(fpw), 0);
-	  //send(newsockfd, fpw, strlen(fpw), 0);
-      close(newsockfd);    
-	  
-   close(sockfd);    
 
+/*---------------------------------------------------------------------------*/
+   
+   
+   
+   
+   
+   
+   /* Write a response to the client */
+   n = write(newsockfd,"Message received",18);
+   
+   if (n < 0) {
+      printf("ERROR writing to socket");
+      exit(1);
+   }
+      
    return 0;
 }
